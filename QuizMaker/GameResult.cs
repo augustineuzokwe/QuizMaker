1487cf4b1b43891c8cc84ab3c6be5de89d75b7e6
Augustine Uzokwe <augustine.uzokwe@gmail.com>
2021-05-31 21:10:23 +0200

    add game result class

---
+++ QuizMaker/GameResult.cs
@@ -0,0 +1,16 @@
+﻿namespace QuizMaker
+{
+    public class GameResult
+    {
+
+        public static bool GameOver { get; set; }
+
+        public enum Result
+        {
+            win,
+            lost,
+            correctAnswer,
+            wrongAnswer
+        }
+    }
+}
