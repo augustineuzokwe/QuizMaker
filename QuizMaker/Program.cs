2067f052033e66c1c73857ee4c187a2a6934dac7
Augustine Uzokwe <augustine.uzokwe@gmail.com>
2021-06-08 12:31:39 +0200

    cleaned up game core

--- QuizMaker/Program.cs
+++ QuizMaker/Program.cs
@@ -12,16 +12,34 @@ namespace QuizMaker
             int answersForEachQuestion = 3;
             DataSerializer<List<QuizDataModel>> dataSerializer = new DataSerializer<List<QuizDataModel>>();
             List<QuizDataModel> quizCards = new List<QuizDataModel>();
+            List<Answer> answers;
+
             int answeredQuestionCount = 0;
 
-            //Create quiz cards
-            for (int x = 0; x < quizCardsToPlay; x++)
-            {
-                quizCards.Add(QuizCore.CreateGameCards(answersForEachQuestion));
-            }
+            ////Create quiz cards
+            //for (int x = 0; x < quizCardsToPlay; x++)
+            //{
+            //    string question = QuizUI.getPlayerQuestion();
+            //    answers = new List<Answer>();
 
-            //Store in serialized xml file
-            dataSerializer.XmlSerialize(quizCards, filePath);
+            //    for (int a = 0; a < answersForEachQuestion; a++)
+            //    {
+            //        string answer = QuizUI.getPlayerAnswerToQuestion();
+
+            //        if (QuizUI.IsCorrectAnswerToQuestion())
+            //        {
+            //            answers.Add(new Answer(answer, true));
+            //        }
+            //        else
+            //        {
+            //            answers.Add(new Answer(answer, false));
+            //        }
+            //    }
+            //    quizCards.Add(new QuizDataModel(question, answers));
+            //}
+
+            ////Store the quiz cards in serialized xml file
+            //dataSerializer.XmlSerialize(quizCards, filePath);
 
             Console.Clear();
 
@@ -31,12 +49,41 @@ namespace QuizMaker
             //Play quiz
             foreach (var gameCard in questionAndAnswers)
             {
-                QuizUI.WelcomeToQuizGame(gameCard.Question);
-
                 answeredQuestionCount++;
+                int wrongAnswersCount = 0;
 
-                //Play quiz
-                QuizCore.QuizCardPlay(gameCard);
+                string answer = QuizUI.EnterAnswerToQuizGame(gameCard);
+
+                for (int x = 0; x < gameCard.Answers.Count; x++)
+                {
+
+                    //Play quiz
+                    var answerResult = QuizCore.QuizCardPlay(gameCard, answer);
+
+                    if (answerResult != QuizResult.Result.correctAnswer)
+                    {
+                        wrongAnswersCount++;
+
+                        if (!wrongAnswersCount.Equals(gameCard.Answers.Count))
+                        {
+                            QuizUI.GameRoundResult(answerResult, 0, answer);
+                            answer = QuizUI.TryAgain();
+                        }
+
+                        continue;
+                    }
+                    else
+                    {
+                        if (answerResult.Equals(QuizResult.Result.correctAnswer))
+                        {
+                            QuizUI.GameRoundResult(answerResult, 0, answer);
+                            break;
+                        }
+                    }
+                }
+
+                // keep track of wrong answers
+                QuizDataModel.QuestionWrongAnswersDictionary.Add(gameCard.Question, wrongAnswersCount);
 
                 if (answeredQuestionCount < questionAndAnswers.Count)
                 {
@@ -45,7 +92,7 @@ namespace QuizMaker
             }
 
             //Display quiz results
-            QuizUI.FinalGameResult(QuizCore.QuestionWrongAnswersDictionary);
+            QuizUI.FinalGameResult(QuizDataModel.QuestionWrongAnswersDictionary);
         }
     }
 }
--- QuizMaker/QuizCore.cs
+++ QuizMaker/QuizCore.cs
@@ -1,20 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using static QuizMaker.QuizResult;
 
 namespace QuizMaker
 {
     public class QuizCore
     {
-        public static Dictionary<string, int> _questionAnsweredWrong = new Dictionary<string, int>();
-        public static bool IsAllQuestionsAnswered { get; set; }
-        private static readonly List<Answer> answers = new List<Answer>();
-
-        public static Dictionary<string, int> QuestionWrongAnswersDictionary
-        {
-            get { return _questionAnsweredWrong; }
-            set { _questionAnsweredWrong = value; }
-        }
-
         public static bool IsCorrectAnswerGiven(QuizDataModel gameCard, string playerAnswer)
         {
             foreach (var item in gameCard.Answers)
@@ -27,48 +16,13 @@ namespace QuizMaker
             return false;
         }
 
-        public static void QuizCardPlay(QuizDataModel gameCard)
+        public static Result QuizCardPlay(QuizDataModel gameCard, string answer)
         {
-            int wrongAnswersCount = 0;
-
-            for (int x = 0; x < gameCard.Answers.Count; x++)
+            if (IsCorrectAnswerGiven(gameCard, answer))
             {
-                string answer = Console.ReadLine();
-
-                if (IsCorrectAnswerGiven(gameCard, answer))
-                {
-                    QuizUI.GameRoundResult(QuizResult.Result.correctAnswer, 0, answer);
-                    break;
-                }
-                else
-                {
-                    QuizUI.GameRoundResult(QuizResult.Result.wrongAnswer, 0, answer);
-                    wrongAnswersCount++;
-                }
+                return Result.correctAnswer;
             }
-
-            // keep track of wrong answers
-            QuestionWrongAnswersDictionary.Add(gameCard.Question, wrongAnswersCount);
-        }
-
-        public static QuizDataModel CreateGameCards(int answersForEachQuestion)
-        {
-            string question = QuizUI.getPlayerQuestion();
-
-            for (int a = 0; a < answersForEachQuestion; a++)
-            {
-                string answer = QuizUI.getPlayerAnswerToQuestion();
-
-                if (QuizUI.IsCorrectAnswerToQuestion())
-                {
-                    answers.Add(new Answer(answer, true));
-                }
-                else
-                {
-                    answers.Add(new Answer(answer, false));
-                }
-            }
-            return new QuizDataModel(question, answers: answers);
+            return Result.wrongAnswer;
         }
     }
 }
